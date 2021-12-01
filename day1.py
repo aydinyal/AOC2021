a58bb6824c479220b4dd3e9ef563175bb94abf11
Aydin Yalçinkaya <aydincodes@gmail.com>
2021-12-01 00:43:36 -0500

    Added Day1

---
+++ day1.py
@@ -0,0 +1,19 @@
+def part1():
+    f = open('day1.txt').read().splitlines()
+    ints = list(map(int, f)) 
+    count = 0
+    for i in range(1, len(ints)):
+        if ints[i] > ints[i-1]:
+            count+= 1
+    print(count)
+
+part1()
+
+def part2():
+    f = open('day1.txt').read().splitlines()
+    ints = list(map(int, f))
+    count = 0
+    for i in range(3, len(ints)):
+        if sum(ints[i-3:i]) < sum(ints[i-2:i+1]): count += 1  
+    print(count)
+part2()
